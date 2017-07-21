deaa64de0398db405fe405ebfed811e6151f981a
mant52 <melbardis@gmail.com>
2017-07-21 11:36:27 +0100

    adding changes

---
+++ index.js
@@ -0,0 +1,19 @@
+const express        = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose       = require('mongoose');
+const bluebird       = require('bluebird');
+const { port, db }   = require('./config/env');
+const routes         = require('./config/routes');
+const app            = express();
+
+mongoose.connect(db);
+mongoose.Promise = bluebird;
+
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
+
+app.use(express.static(`${__dirname}/public`));
+app.use(expressLayouts);
+app.use(routes);
+
+app.listen(port, () => console.log(`Express up and running on port: ${port}`));
